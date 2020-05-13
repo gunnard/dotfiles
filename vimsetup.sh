fcebf3536d5d7f9420f86a70700dbfc9f4e401f2
Gunnard Engebreth <gunnard@gmail.com>
2020-05-12 20:18:51 -0500

    added vimsetup.sh

---
+++ vimsetup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+cd ~/.vim/bundle
+git clone https://github.com/mattn/emmet-vim.git
+mkdir ~/.vim/autoload
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir ~/.vim/colors && cd ~/.vim/colors
+wget https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+mv jellybeans.vim ~/.vim/colors
